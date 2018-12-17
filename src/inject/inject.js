a58dac90874ebcb0d9616f3c8538206abbebdbc1
jahoops <jwhooper@gmail.com>
2018-12-16 21:52:56 -0500

    up

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,13 +1,24 @@
 chrome.extension.sendMessage({}, function(response) {
+	var clips = [];
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
-
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
-
+		document.addEventListener("click", function(e){
+			var shift = e.shiftKey;
+			var ctrl = e.ctrlKey;
+			switch(true) {
+				case shift && ctrl:
+				console.log(JSON.stringify(clips));
+				break;
+				case shift:
+				clips.push('Q:' + e.target.innerHTML);
+				e.target.style.color = "red";
+				break;
+				case ctrl:
+				clips.push('A:' + e.target.innerHTML);
+				e.target.style.color = "green";
+			}
+		});
 	}
 	}, 10);
 });
